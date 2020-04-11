3d5cc84f07430c6f5cdb9ac4063c41d50f8683e7
Dennis Phetsomphou <dennisph@my.yorku.ca>
2020-04-11 19:29:13 -0400

    Init (Raycast, Lock + Rotate Cubes)
    Locks Cube into a Plane (GameObject) using Raycasts for Rotating them all together.

---
+++ LocalDirections.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalDirections : MonoBehaviour
+{
+    public Transform trans;
+
+    public Vector3 N { get { return trans.TransformDirection(Vector3.up); } } 
+    public Vector3 NE { get { return trans.TransformDirection(Vector3.up + Vector3.right); } }
+    public Vector3 E { get { return trans.TransformDirection(Vector3.right); } }
+    public Vector3 SE { get { return trans.TransformDirection(-Vector3.up + Vector3.right); } }
+    public Vector3 S { get { return trans.TransformDirection(-Vector3.up); } }
+    public Vector3 SW { get { return trans.TransformDirection(-Vector3.up + -Vector3.right); } }
+    public Vector3 W { get { return trans.TransformDirection(-Vector3.right); } }
+    public Vector3 NW { get { return trans.TransformDirection(Vector3.up + -Vector3.right); } }
+
+    public Vector3[] AllDirections(){
+        return new Vector3[]{ N, NE, E, SE, S, SW, W, NW }; } 
+
+    public LocalDirections(Transform t) {
+        trans = t;
+    }
+
+
+}
---
+++ RotateOnAxis.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateOnAxis : MonoBehaviour
+{
+
+    public List<GameObject> cubes;
+    public Vector3 rotation;
+    public bool isLocked = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LocalDirections ld = new LocalDirections(this.transform);
+        Debug.DrawRay(ld.trans.position, ld.N * 20, Color.yellow);
+        Debug.DrawRay(ld.trans.position, ld.NE * 20, Color.yellow);
+        Debug.DrawRay(ld.trans.position, ld.E * 20, Color.yellow);
+        Debug.DrawRay(ld.trans.position, ld.SE * 20, Color.yellow);
+        Debug.DrawRay(ld.trans.position, ld.S * 20, Color.yellow);
+        Debug.DrawRay(ld.trans.position, ld.SW * 20, Color.yellow);
+        Debug.DrawRay(ld.trans.position, ld.W * 20, Color.yellow);
+        Debug.DrawRay(ld.trans.position, ld.NW * 20, Color.yellow);
+
+        //When touched then initiate lock
+        RaycastHit hit;
+        foreach (Vector3 dir in ld.AllDirections()) {
+            if (Physics.Raycast(ld.trans.position, dir, out hit, 20f))
+            {
+                Debug.Log("Hit: " + hit.transform);
+                if(!cubes.Contains(hit.transform.gameObject))
+                cubes.Add(hit.transform.gameObject);
+            }
+            else {
+                Debug.Log("Miss");
+            }
+        }
+
+        this.transform.rotation = Quaternion.Euler(rotation);
+        LockCubes();
+    }
+
+    void LockCubes() {
+        foreach (GameObject cube in cubes) {
+            cube.transform.SetParent(this.gameObject.transform);
+        }
+    }
+}
