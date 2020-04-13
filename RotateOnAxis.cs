315159d435e650f9507429b043ba96f18457f6c0
Dennis Phetsomphou <dennisph@my.yorku.ca>
2020-04-13 17:48:46 -0400

    Strict 90 Rotation and Locks (Temp)

--- RotateOnAxis.cs
+++ RotateOnAxis.cs
@@ -8,10 +8,13 @@ public class RotateOnAxis : MonoBehaviour
     public List<GameObject> cubes;
     public Vector3 rotation;
     public bool isLocked = true;
+    public bool isRotating = false;
+    public Transform Rubik;
+    public Rubik rubik;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rubik = GetComponentInParent<Rubik>();
     }
 
     // Update is called once per frame
@@ -28,26 +31,93 @@ public class RotateOnAxis : MonoBehaviour
         Debug.DrawRay(ld.trans.position, ld.NW * 20, Color.yellow);
 
         //When touched then initiate lock
-        RaycastHit hit;
-        foreach (Vector3 dir in ld.AllDirections()) {
-            if (Physics.Raycast(ld.trans.position, dir, out hit, 20f))
-            {
-                Debug.Log("Hit: " + hit.transform);
-                if(!cubes.Contains(hit.transform.gameObject))
-                cubes.Add(hit.transform.gameObject);
-            }
-            else {
-                Debug.Log("Miss");
-            }
-        }
-
-        this.transform.rotation = Quaternion.Euler(rotation);
-        LockCubes();
+        //this.transform.rotation = Quaternion.Euler(rotation);
     }
 
     void LockCubes() {
         foreach (GameObject cube in cubes) {
             cube.transform.SetParent(this.gameObject.transform);
+
+            Cube c;
+            if (cube.TryGetComponent<Cube>(out c)) {
+                c.ClearAxis();
+            }
+        }
+        isLocked = true;
+    }
+
+    void UnlockCubes() {
+        foreach(GameObject cube in cubes) {
+            cube.transform.SetParent(Rubik);
+        }
+        /* Need to Clear Cubes That are Rotatable Otherwise they get caught by other axis*/
+        isLocked = false;
+        rubik.UpdateAllAxis();
+    }
+
+    
+    public void RotateAxis() { 
+        //Vector3 newRotation = this.transform.rotation.eulerAngles + new Vector3(0, 0, 90f);
+        //this.transform.rotation = Quaternion.Euler(newRotation);
+        if (!isRotating)
+        {
+            isRotating = true;
+            StartCoroutine(Rotate(new Vector3(0, 0, 1), 90, 1.0f));
         }
     }
+
+    public void UpdateAxis() {
+        Debug.Log("Updating Axis");
+        cubes.Clear();
+        RaycastHit hit;
+        LocalDirections ld = new LocalDirections(this.transform);
+        foreach (Vector3 dir in ld.AllDirections())
+        {
+            if (Physics.Raycast(ld.trans.position, dir, out hit, 20f))
+            {
+                Cube cube;
+                if (hit.transform.gameObject.TryGetComponent<Cube>(out cube))
+                {
+                    if (!cubes.Contains(hit.transform.gameObject))
+                        cubes.Add(hit.transform.gameObject);
+
+                    cube.AddAxis(this);
+                }
+                else
+                {
+                    Debug.Log("Object missing Cube Script");
+                }
+
+            }
+            else
+            {
+                Debug.Log("Miss");
+            }
+        }
+
+    }
+
+    IEnumerator Rotate(Vector3 axis, float angle, float duration = 1.0f)
+    {
+        LockCubes();
+        Debug.Log("Start");
+        Quaternion from = transform.rotation;
+        Quaternion to = transform.rotation;
+        to *= Quaternion.Euler(axis * angle);
+
+        float elapsed = 0.0f;
+        while (elapsed < duration)
+        {
+            transform.rotation = Quaternion.Slerp(from, to, elapsed / duration);
+            elapsed += Time.deltaTime;
+            
+            yield return null;
+            
+        }
+        
+        transform.rotation = to;
+        UnlockCubes();
+        isRotating = false;
+    }
+
 }
