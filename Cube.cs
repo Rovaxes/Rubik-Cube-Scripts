a753561d24ff4d4f9090dfd35b04c06ac31d4534
Dennis Phetsomphou <dennisph@my.yorku.ca>
2020-04-13 17:48:13 -0400

    Init Cube Class
    Mainly used to keep track of which axis it can rotate on

---
+++ Cube.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+    public List<RotateOnAxis> RotatatableAxis;
+    public bool rotate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //LocalDirections ld = new LocalDirections(this.transform);      
+        if (rotate) {
+            RotatatableAxis[0].RotateAxis();
+        }
+    }
+
+
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("OnDrag");
+    }
+
+    public void ClearAxis() {
+        RotatatableAxis.Clear();
+    }
+    public void AddAxis(RotateOnAxis roa) {
+        if (!RotatatableAxis.Contains(roa))
+        {
+            RotatatableAxis.Add(roa);
+        }
+    }
+}
