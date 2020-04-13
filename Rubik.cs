047b014df30b5994df6658964771b224de4a5b4f
Dennis Phetsomphou <dennisph@my.yorku.ca>
2020-04-13 17:49:05 -0400

    Parent Cube for Updates

---
+++ Rubik.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rubik : MonoBehaviour
+{
+    public RotateOnAxis[] faces;
+    public Cube[] cubes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        faces = GetComponentsInChildren<RotateOnAxis>();
+        cubes = GetComponentsInChildren<Cube>();
+        UpdateAllAxis();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateAllAxis() {
+
+        foreach (RotateOnAxis roa in faces) {
+            roa.UpdateAxis();           
+        }
+    }
+}
